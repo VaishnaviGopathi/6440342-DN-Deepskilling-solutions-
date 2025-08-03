9a5cf6529dedbbc266feb17593d54c6b78448778
VaishnaviGopathi <vaishnavigopathi2005@gmail.com>
2025-08-03 13:41:03 +0530

    Add files via upload

---
+++ DN WEEK-7/13.ReactJS-HOL/components/CourseDetails.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function CourseDetails({ show }) {
+  if (!show) return null;
+
+  const courses = [
+    { name: 'Angular', date: '4/5/2021' },
+    { name: 'React', date: '6/3/2021' }
+  ];
+
+  return (
+    <div style={{ flex: 1 }}>
+      <h2>Course Details</h2>
+      {courses.map((course, index) => (
+        <div key={index}>
+          <h3>{course.name}</h3>
+          <p>{course.date}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default CourseDetails;
